310cf4f2b0acad99260dc87418af42d3029f371a
wolfet410 <wolfet410@gmail.com>
2015-04-30 17:13:42 -0400

    Testing walk

---
+++ build.cmd
@@ -0,0 +1,3 @@
+rem Note the manifest file must exist with the executable unless it's compiled differently (not below)
+
+go build -ldflags="-H windowsgui"
---
+++ dtcnotif.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "github.com/lxn/walk"
+    . "github.com/lxn/walk/declarative"
+    "strings"
+)
+
+func main() {
+    var inTE, outTE *walk.TextEdit
+
+    MainWindow{
+        Title:   "User Generated Title",
+        MinSize: Size{600, 400},
+        Layout:  VBox{},
+        Children: []Widget{
+            HSplitter{
+                Children: []Widget{
+                    TextEdit{AssignTo: &inTE},
+                    TextEdit{AssignTo: &outTE, ReadOnly: true},
+                },
+            },
+            PushButton{
+                Text: "OK",
+                OnClicked: func() {
+                    outTE.SetText(strings.ToUpper(inTE.Text()))
+                },
+            },
+        },
+    }.Run()
+}
